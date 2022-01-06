580a877c7f2b49ca5afad5ac6d4563d216bea38d
evanwb <evanwburton@gmail.com>
2022-01-06 15:23:29 -0500

    moved html to react and added styling

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Login from './components/Login.js'
+
+function App() {
+  return (
+    <html>
+      <body>
+        <div>
+          Spacer 
+        </div>
+      <Login/>
+
+      </body>
+      
+    </html>
+   
+  );
+}
+
+export default App;
