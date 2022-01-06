7ce0c7f5be91c925554cff11a27641bb41d332a6
Decker <jdb3qn@virginia.edu>
2022-01-06 15:30:39 -0500

    Making sure i have it

--- c1hackathon/src/App.js
+++ c1hackathon/src/App.js
@@ -1,15 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import SavingsAccount from './Components/SavingsAccount';
+import { BrowserRouter as Router, Switch, Route, Link, useParams } from "react-router-dom"
+import { Navigate } from 'react-router-dom'
+
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Accounts</h1>
-      <SavingsAccount/>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div className="App">
+            <h1>Accounts</h1>
+            <SavingsAccount name="decker" />
+          </div>
+        </Route>
+
+        <Route exact path="/savingsAccount/:id">
+          <h1>Routing Works</h1>
+
+        </Route>
+      </Switch>
+    </Router>
 
 
-    </div>
   );
 }
 
--- c1hackathon/src/Components/SavingsAccount.js
+++ c1hackathon/src/Components/SavingsAccount.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
 import "./SavingsAccountStyle.css"
+import { BrowserRouter as Router, Switch, Route, Link, useParams } from "react-router-dom"
 
 function SavingsAccount (props){
 
 
 
-    return <div className="savingsAccount">
+
+    return <div className="savingsAccount" >
+        {/* <Link to={"/savingsAccount/"+props.name}></Link> */}
+
 
         <h1>Capital One</h1>
-        <p>This is a savings account</p>
+        <p>This is a savings account for {props.name}</p>
 
     </div>
 
