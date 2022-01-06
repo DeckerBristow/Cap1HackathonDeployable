66e2fea6daf8d571ef9341b58190b01b9a27c9e6
Decker <jdb3qn@virginia.edu>
2022-01-06 13:19:56 -0500

    Added a savings account page/component

---
+++ c1hackathon/src/Components/SavingsAccount.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import "./SavingsAccountStyle.css"
+
+function SavingsAccount (props){
+
+
+
+    return <div className="savingsAccount">
+
+        <h1>Capital One</h1>
+        <p>This is a savings account</p>
+
+    </div>
+
+};
+
+export default SavingsAccount;
