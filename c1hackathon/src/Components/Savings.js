449faef0548949142ab122012e9aeeada6428e71
Joe Khosbayar <qbk5th@virginia.edu>
2022-01-07 00:13:42 -0500

    state added to savings

--- c1hackathon/src/Components/Savings.js
+++ c1hackathon/src/Components/Savings.js
@@ -3,14 +3,31 @@ import React, {useState, useEffect} from 'react'
 import Logo from './c1logo.png'
 import { BrowserRouter as Router, Switch, Route, Link, useParams } from "react-router-dom"
 import ReactSvgPieChart from "react-svg-piechart"
-import firestore from '../firebase'
-import { collection, getDocs } from "firebase/firestore/lite"
+import { initializeApp } from 'firebase/app';
+import firestore from "../firebase"
+import { collection, getDocs } from 'firebase/firestore/lite';
+//import { useNavigate } from 'react-router-dom'
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJuFa43Y6y0roJK26I0M-he_x8k1sWa5g",
+  authDomain: "poptropica-99ff3.firebaseapp.com",
+  projectId: "poptropica-99ff3",
+  storageBucket: "poptropica-99ff3.appspot.com",
+  messagingSenderId: "474378189484",
+  appId: "1:474378189484:web:7b074ad60aad348e193d09",
+  measurementId: "G-TQK5SEWGDY"
+};
+
+
 
 const Savings = (props) => {
-    const name = "John Doe" //Will use get request
-    const balance = "$200" //will use get request
-    
-    const data = [//example data
+    const [name, setName] = useState('')
+    const [balance, setBalance] = useState('')
+    const app = initializeApp(firebaseConfig);
+
+    const data = [  //example data
         {title: "Data 1", value: 10, color: "#AF454A"},
         {title: "Data 2", value: 10, color: "#CE14F7"},
         {title: "Data 3", value: 30, color: "#3da18d"},
@@ -20,41 +37,68 @@ const Savings = (props) => {
 
     const [savingsName, setSavingsName] = useState(''); //state for extracting input name
     const [amount, setAmount] = useState('');   //state for extracting input amount
+    const [accounts, setAccounts] = useState([])
+    const [uid,setUid] = useState('')
 
     const nameChangeHandler = (event) => {
-        console.log(event.target.value)
+        //console.log(event.target.value)
         setSavingsName(event.target.value)
     }
 
     const amountChangeHandler = (event) => {
-        console.log(event.target.value)
+        //console.log(event.target.value)
         setAmount(event.target.value)
     }
+    //try to use connect to firestorm db
+    useEffect(() => {
+        let accounts = collection(firestore, "savingsAccounts");
+        getDocs(accounts).then(snapshot => {
+          //Snapshots is just an array of all the documents in blog posts
+          let tempAccounts = [];
+          
+          snapshot.forEach(document => {
+            tempAccounts.push(document.data());
+          });
+      
+          setAccounts(tempAccounts);
+          accounts.forEach(savings => {
+            if(savings['userid'] == uid){ //uid needs to be passed from login somehow
+                setBalance(savings['balance']);
+                setName(savings['name']);
+            }
+          });
+          
+          
+        });
 
-    const addGoal = ()=>{ //takes inputs when button is pressed and sends POST request to firebase
-        console.log('click')
+        
+      }, []);
+    const AddGoal = ()=>{ //takes inputs when button is pressed and sends POST request to firebase
+        //console.log('click')
         const newGoal = {
            name: savingsName,
            amount: amount,
         }
-        console.log(newGoal)
+        //console.log(newGoal)
         
-        //try to use connect to firestorm db
-        // useEffect(()=>{
-        //     const 
-        // },[])
+        
     }
+    accounts.forEach(element => {if(element.userid == uid){
+        console.log(element.userid)
+    }});
+    
     //console.log(savingsName)
     //console.log(amount);
+    //console.log(accounts[0].Goals);
     return (
         <div>
             <div className = 'header'>
-                <h1>Hello  {name}</h1>
+                <h1 className = 'hello'>Hello  {name}</h1>
                 <div className = 'page'>
                     <img src = {Logo} alt = "c1"/>
                 </div>
             </div>
-        <h2>Current Balance: {balance}</h2>
+        <h2 className = 'balance'>Current Balance: {balance}</h2>
         <h3>Savings Goals</h3>
 
                 <div className = 'addgoal'>
@@ -62,7 +106,7 @@ const Savings = (props) => {
                     <input placeholder = 'Type Goal Name Here' onChange = {nameChangeHandler}/>
                     <label>Amount</label>
                     <input type="text" pattern="[0-9]*" placeholder = 'Type Amount' onChange = {amountChangeHandler}/>
-                    <button onClick = {addGoal} >Add Goal</button>
+                    <button onClick = {AddGoal} >Add Goal</button>
                 </div>
                 <div className="list-container">
 
@@ -83,7 +127,7 @@ const Savings = (props) => {
                 </div>
 
 
-        <h3><Link to={"/landingPage/"}>Back</Link></h3>
+        <h3 className = 'back'><Link to={"/landingPage/"}>Back</Link></h3>
 
 
         </div>
